a95af71071638771a473bcd2d5e1116083570b04
rahulkate173 <rahulkate173@gmail.com>
2026-01-24 11:03:04 +0530

    commit structure

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+from flask import Flask , render_template,redirect,url_for,request
+import uvicorn 
+
+app = Flask(__name__)
+
+@app.route("/check")
 def main():
-    print("Hello from campus-connect!")
+    return {""
+    "response":"Hello from campus-connect",
+    "status":200
+    }
+
+@app.route("/")
+def home():
+    """Home-page"""
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
-    main()
+    app.run(host="localhost",port=8000,debug=True)
+    
